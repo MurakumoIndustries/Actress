d05020deb7d270cecfbf7b5bc0e048019dc6a12e
WindWT <leibao0723@gmail.com>
2018-05-16 22:52:32 +0800

    update to latest

--- src/js/data.js
+++ src/js/data.js
@@ -2,7 +2,7 @@ import localForage from "localforage";
 
 var data = {};
 
-const version = 20656;
+const version = 20744;
 var getVersion = function () { return version; };
 
 const baseKey = "MI_Actress_";
