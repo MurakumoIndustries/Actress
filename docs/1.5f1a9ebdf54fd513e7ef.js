05c7b6d1240d1d13cb214126216bddf796771224
WindWT <leibao0723@gmail.com>
2018-09-12 19:28:21 +0800

    update to latest

--- docs/1.5f1a9ebdf54fd513e7ef.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([1],{26:function(n,o){n.exports="2018-09-11T19:57:35.0257409+08:00"}});
--- src/js/data.js
+++ src/js/data.js
@@ -2,7 +2,7 @@ import localForage from "localforage";
 
 var data = {};
 
-const version = 21351;
+const version = 21378;
 var getVersion = function () { return version; };
 
 const baseKey = "MI_Actress_";
